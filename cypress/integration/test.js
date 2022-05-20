96bc05382e3263808891d6ea59d8cb1ef1234cb9
Dmytro Pelyp <pelup19992@gmail.com>
2022-05-20 15:01:06 +0300

    added tests

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,6 +1,25 @@
-import * as testData from '../support/testData.js'
-import {pageElements} from '../support/locators.js'
+{/* <reference types="cypress" /> */}
+
+import {PageElements} from '../support/locators.js';
+import faker from 'faker';
 var generator = require('generate-password');
+
+const pe = new PageElements()
+
+const RANDOM_NAME = faker.name.findName();
+const RANDOM_LAST_NAME = faker.name.lastName();
+const RANDOM_EMAIL = faker.internet.email();
+const RANDOM_ADDRESS = faker.address.streetAddress();
+const RANDOM_PHONE_NUMBER = faker.datatype.number({
+  'min': 100000000,
+  'max': 999999999
+});
+const RANDOM_EMAIL_QUICK_FORM = faker.internet.email()
+const RANDOM_PHONE_NUMBER_QUICK_FORM = faker.datatype.number({
+  'min': 100000000,
+  'max': 999999999
+});
+
 
 var password = generator.generate({
 	length: 10,
@@ -10,30 +29,112 @@ var password = generator.generate({
   symbols : false
 });
 
-export function randomCountry () {
-  const myArray = ["Ukraine", "Uganda", "Spain"];
-  const rand = Math.floor(Math.random()*myArray.length);
-  return myArray[rand];
+function randomCountry () {
+  const countries = ["784", "809", "810"];
+  const rand = Math.floor(Math.random()*countries.length);
+  return countries[rand];
   }
+
+  function randomAge () {
+    const years = ["1999", "2000", "2001"]; 
+    const rand = Math.floor(Math.random()*years.length);
+    return years[rand];
+  }
+
+  function randomMonths () {
+    const months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']; 
+    const rand = Math.floor(Math.random()*months.length);
+    return months[rand];
+  }
+
+  function randomDay () {
+    const days = ['1', '2', '3', '4', '5']; 
+    const rand = Math.floor(Math.random()*days.length);
+    return days[rand];
+  }
+
+  function randomGender () {
+    const gender = ['1', '2']; 
+    const rand = Math.floor(Math.random()*gender.length);
+    return gender[rand];
+  }
+
+  function randomCountrySecond () {
+    const countries = ["784", "809", "810"];
+    const rand = Math.floor(Math.random()*countries.length);
+    return countries[rand];
+    }
+
+    function randomCurrency () {
+      const currency = ["USD", "EUR", "UYU", "RUB"];
+      const rand = Math.floor(Math.random()*currency.length);
+      return currency[rand];
+      }
+  
+
 
 describe('User registartaion Full', () => {
     before (()=> {
       cy.visit(Cypress.env("baseURL"))});
-      const pe = new Elements()
+
     it('Form 1 Registration', () => {
+
       cy.get(pe.registrationButton).click().should('be.visible'),
-      cy.get('.form-controls-container > :nth-child(1) > .form_field > .ng-untouched').click().should('be.visible')
-      .type(testData.RANDOM_NAME),
-      cy.get('.form-controls-container > :nth-child(2) > .form_field > .ng-untouched').click().should('be.visible')
-      .type(testData.RANDOM_EMAIL),
-      cy.get(':nth-child(1) > .form_field > .ng-untouched').click().should('be.visible').type(password).should('have.value', password),
-      cy.get('.form_field > .ng-pristine').click({ multiple: true }).should('be.visible').type(password, { multiple: true }).should('have.value', password),
-      cy.get('.custom_dropdown_reg').select(randomCountry()).should('be.visible')
-      cy.get('.craft_btn').should('be.visible').click()
+      cy.get(pe.userName).click().should('be.visible').type(RANDOM_NAME),
+      cy.get(pe.email).click().should('be.visible').type(RANDOM_EMAIL),
+      cy.get(pe.password).click().should('be.visible').type(password).should('have.value', password),
+      cy.get(pe.confirmPassword).click({ multiple: true }).should('be.visible').type(password, { multiple: true }).should('have.value', password),
+      cy.get(pe.country).select(randomCountry()).should('be.visible')
+      cy.get(pe.nextButton).should('be.visible').click()
   })
 
-//   it('Form 2 Registration', () => {
-//     cy.get(':nth-child(1) > .form_field > .ng-untouched').type(ok).should('have.value', ok)
-// })
+  it('Form 2 Registration', () => {
+    cy.get(pe.firstName).type(RANDOM_NAME).should('have.value', RANDOM_NAME),
+    cy.get(pe.lastName).type(RANDOM_LAST_NAME).should('have.value', RANDOM_LAST_NAME),
+    cy.get(pe.birthDate).select(randomAge()).should('be.visible'),
+    cy.get(pe.birthMonths).select(randomMonths()).should('be.visible'),
+    cy.get(pe.birthDay).select(randomDay()).should('be.visible'),
+    cy.get(pe.gender).select(randomGender()).should('be.visible'),
+    cy.get(pe.country).select(randomCountrySecond()).should('be.visible'),
+    cy.get(pe.city).select('Select City').should('be.visible'),
+    cy.get(pe.nextButtonSecond).should('be.visible').click()
+}) 
+it('Form 3 Registration', () => {
+ cy.get(pe.address).click().should('be.visible').type(RANDOM_ADDRESS).should('have.value', RANDOM_ADDRESS),
+ cy.get(pe.currency).select(randomCurrency()).should('be.visible'),
+ cy.get(pe.countryCode).should("be.visible"),
+ cy.get(pe.phoneNumber).click().should('be.visible').type(RANDOM_PHONE_NUMBER),
+ cy.get(pe.checkbox).click().should('be.checked'),
+ cy.get(pe.registerButton).should('be.visible').click()
+})
 });
+
+describe('User registartaion Quick', () => {
+  before (()=> {
+    cy.visit(Cypress.env("baseURL"))});
+
+  it('Email Registration Form', () => {
+    cy.get(pe.registrationButton).should('be.visible').click(),
+    cy.get(pe.quickFormButton).should('be.visible').click(),
+    cy.get(pe.emailCheckBox).click().should('be.checked'),
+    cy.get(pe.email).click().should('be.visible').type(RANDOM_EMAIL_QUICK_FORM),
+    cy.get(pe.currency).select(randomCurrency()).should('be.visible'),
+    cy.get(pe.checkbox).click().should('be.checked'),
+    cy.get(pe.registerButton).should('be.visible').click()
+  })
+
+  it('Phone Registration Form', () => {
+    cy.get(pe.userProfile).should('be.visible').click(),
+    cy.get(pe.logoutButton).should('be.visible').click(),
+    cy.get(pe.registrationButton).should('be.visible').click(),
+    cy.get(pe.quickFormButton).should('be.visible').click(),
+    cy.get(pe.phoneCheckBox).click().should('be.checked'),
+    cy.get(pe.countryCode).should("be.visible"),
+    cy.get(pe.phoneNumber).click().should('be.visible').type(RANDOM_PHONE_NUMBER_QUICK_FORM),
+    cy.get(pe.currency).select(randomCurrency()).should('be.visible'),
+    cy.get(pe.checkbox).click().should('be.checked'),
+    cy.get(pe.registerButton).should('be.visible').click()
+  })
+})
+
 
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -6,5 +6,26 @@ export class PageElements {
     email = '.form-controls-container > :nth-child(2) > .form_field > .ng-untouched';
     password = ':nth-child(1) > .form_field > .ng-untouched';
     confirmPassword = '.form_field > .ng-pristine';
-    
+    country = '.custom_dropdown_reg';
+    nextButton = '.craft_btn';
+    firstName = ':nth-child(1) > .form_field > .ng-untouched';
+    lastName = '.form_field > .ng-pristine';
+    birthDate = '.birthDate_tabs > :nth-child(1) > .custom_dropdown_reg';
+    birthMonths = '#birth-month';
+    birthDay = ':nth-child(3) > .ng-untouched';
+    gender = '.form_field > .select-box > .custom_dropdown_reg';
+    country = '.begion_tabs > :nth-child(1) > .custom_dropdown_reg';
+    city = '.begion_tabs > :nth-child(2) > .custom_dropdown_reg';
+    nextButtonSecond = '.next-btn';
+    address ='.form_field > .ng-pristine';
+    currency = '.select-box > .ng-pristine';
+    countryCode = '.flex_between';
+    phoneNumber = '.mobileData_tabs > .ng-untouched';
+    checkbox = '.reg-checkbox';
+    registerButton = '.next-btn';
+    quickFormButton = '.tabs-container > :nth-child(2)';
+    emailCheckBox = '#r_0';
+    userProfile = '.mobile-header-section_topPanel_right > .icon-profile';
+    logoutButton = '.mobile-internal > .flex_container > .flex_child > .bottom > .logout_button > .general-menu_item';
+    phoneCheckBox = '#r_1';
 }
