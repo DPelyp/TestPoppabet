9a02d00f550f63d4e95a37f0d2c375319a3d867e
Dmytro Pelyp <pelup19992@gmail.com>
2022-05-21 18:13:01 +0300

    deleted extra files

--- cypress/support/testData.js
+++
@@ -1,5 +0,0 @@
-import faker from 'faker';
-
-export const RANDOM_NAME = faker.name.findName();
-export const RANDOM_LAST_NAME = faker.name.lastName();
-export const RANDOM_EMAIL = faker.internet.email();
